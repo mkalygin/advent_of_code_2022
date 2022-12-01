86cbb3a7564d8b51d550ed7ff31a58db09ab294c
Michael Kalygin <michael.kalygin@gmail.com>
2022-12-01 18:19:49 +0400

    Fix typo

--- 01a.exs
+++ 01a.exs
@@ -2,8 +2,8 @@ File.stream!("./inputs/01.txt")
 |> Stream.map(&String.trim_trailing/1)
 |> Stream.chunk_by(&(&1 !== ""))
 |> Stream.filter(&(hd(&1) !== ""))
-|> Stream.map(fn callories ->
-  callories
+|> Stream.map(fn calories ->
+  calories
   |> Enum.map(&String.to_integer/1)
   |> Enum.sum()
 end)
